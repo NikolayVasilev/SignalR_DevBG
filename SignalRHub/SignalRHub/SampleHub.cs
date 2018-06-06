59ad3ae310bd129c8e55c13b7acc3d874fd0b588
Nikolay Vasilev <n.vasilev.fmi@gmail.com>
2018-06-06 13:11:04 +0300

    initial commit

---
+++ SignalRHub/SignalRHub/SampleHub.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace SignalRHub
+{
+    public class SampleHub : Hub
+    {
+        private static Dictionary<string, string> userNames = new Dictionary<string, string>();
+
+        public void Send(string name, string message)
+        {
+            if (message.StartsWith("@"))
+            {
+                var splits = message.Split(new char[] { ' ' });
+
+                if(splits.Length > 0)
+                {
+                    var userName = splits[0].Substring(1);
+                    var messageText = message.Substring(splits[0].Length + 1);
+
+                    Clients.Client(userNames[userName]).broadcastMessage(name, messageText);
+                    if (name != userName)
+                    {
+                        Clients.Client(userNames[name]).broadcastMessage(name, messageText);
+                    }
+                }
+            }
+            else
+            {
+                Clients.All.broadcastMessage(name, message);
+            }
+        }
+
+        public void Register(string userName)
+        {
+            if (!userNames.ContainsKey(userName))
+            {
+                userNames.Add(userName, Context.ConnectionId);
+            }
+        }
+
+        public override Task OnConnected()
+        {
+            var id = Context.ConnectionId;
+
+            return base.OnConnected();
+        }
+
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            var userName = string.Empty;
+
+            foreach (var key in userNames.Keys)
+            {
+                if(userNames[key] == Context.ConnectionId)
+                {
+                    userName = key;
+                }
+            }
+
+            userNames.Remove(userName);
+
+            return base.OnDisconnected(stopCalled);
+        }
+    }
+}
---
+++ SignalRHub/SignalRHub/Startup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(SignalRHub.Startup))]
+
+namespace SignalRHub
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+            var config = new HubConfiguration();
+            config.EnableJSONP = true;
+            app.MapSignalR(config);
+        }
+    }
+}
